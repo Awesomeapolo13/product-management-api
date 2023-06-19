622e89604cb7b3866cd260d95b417e7a502745b1
ALogachev <aelof@yandex.ru>
2023-06-19 21:00:29 +0300

    dev/project_initialization The jwt auth has been completed

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,7 +7,8 @@ import { ConfigServiceInterface } from '../common/config/config.service.interfac
 import { BaseController } from '../common/http/base.controller';
 import { UserService } from './user.service';
 import { UserRegisterDto } from './dto/user.register.dto';
-import { BaseMsgEnum } from '../common/dictionary/base.msg.enum';
+import { UserLoginDto } from './dto/user.login.dto';
+import { HttpError } from '../common/error/http.error';
 
 @injectable()
 export class UserController extends BaseController implements UserControllerInterface {
@@ -23,10 +24,27 @@ export class UserController extends BaseController implements UserControllerInte
 				method: 'post',
 				func: this.registerAction,
 			},
+			{
+				path: '/login',
+				method: 'post',
+				func: this.loginAction,
+			},
 		]);
 	}
-	loginAction(req: Request, res: Response, next: NextFunction): void {
-		// ToDo Realize login
+
+	public async loginAction({ body }: Request, res: Response, next: NextFunction): Promise<void> {
+		const dto = new UserLoginDto();
+		dto.email = body.email;
+		dto.password = body.password;
+		try {
+			const jwt = await this.userService.login(dto);
+			this.ok(res, { success: true, jwt: jwt });
+		} catch (e) {
+			if (e instanceof HttpError) {
+				return this.error(next, e.message, e.statusCode);
+			}
+			next(e);
+		}
 	}
 
 	public async registerAction({ body }: Request, res: Response, next: NextFunction): Promise<void> {
@@ -38,10 +56,7 @@ export class UserController extends BaseController implements UserControllerInte
 			const user = await this.userService.register(dto);
 			this.ok(res, { success: true, user: user?.email, id: user?.id });
 		} catch (e) {
-			if (e instanceof Error) {
-				this.logger.error(e.message);
-			}
-			return this.error(next, BaseMsgEnum.DEFAULT_ERR_MGS, 400);
+			return next(e);
 		}
 	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,6 +6,11 @@ import { ConfigServiceInterface } from '../common/config/config.service.interfac
 import { UserRepositoryInterface } from './user.repository.interface';
 import { User } from './user.entity';
 import { Role, UserModel } from '@prisma/client';
+import { UserLoginDto } from './dto/user.login.dto';
+import { sign } from 'jsonwebtoken';
+import { HttpError } from '../common/error/http.error';
+import { HttpStatusCodeEnum } from '../common/http/http.status.code.enum';
+import { hash, compare } from 'bcrypt';
 
 @injectable()
 export class UserService {
@@ -13,9 +18,10 @@ export class UserService {
 		@inject(TYPES.ConfigService) private readonly configService: ConfigServiceInterface,
 		@inject(TYPES.UserRepository) private readonly userRepo: UserRepositoryInterface,
 	) {}
+
 	public async register({ name, email, password }: UserRegisterDto): Promise<UserModel | null> {
 		const newUser = new User(uuidv4(), name, email, Role.ADMIN, new Date());
-		const secret = this.configService.get('SECRET');
+		const secret = this.configService.get('SALT');
 		await newUser.setPassword(password, Number(secret));
 		const existedUser = await this.userRepo.find(email);
 		if (existedUser) {
@@ -23,5 +29,42 @@ export class UserService {
 		}
 
 		return this.userRepo.create(newUser);
+	}
+
+	public async login({ email, password }: UserLoginDto): Promise<string> {
+		const user = await this.userRepo.find(email);
+		const hashedPass = await hash(password, Number(this.configService.get('SALT')));
+		if (!user || (await compare(hashedPass, user.password))) {
+			throw new HttpError(
+				HttpStatusCodeEnum.UNAUTHORIZED_CODE,
+				'Неверный email или пароль.',
+				'Auth',
+			);
+		}
+
+		return await this.signJWT(user.id, user.email, user.role, this.configService.get('SECRET'));
+	}
+
+	private signJWT(id: string, email: string, role: Role, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			sign(
+				{
+					id: id,
+					email: email,
+					role: role,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{
+					algorithm: 'HS256',
+				},
+				(err, token) => {
+					if (err) {
+						reject(err);
+					}
+					resolve(token as string);
+				},
+			);
+		});
 	}
 }
