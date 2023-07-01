fdc58137b9254d0bbadf1c812a8318dc5c5a1273
ALogachev <aelof@yandex.ru>
2023-07-01 13:30:04 +0300

    dev/project_initialization Some fixes for the code review

--- src/user/interface/user.controller.interface.ts
+++ src/user/interface/user.controller.interface.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
-export interface UserControllerInterface {
+export interface IUserController {
 	loginAction: (req: Request, res: Response, next: NextFunction) => void;
 	registerAction: (req: Request, res: Response, next: NextFunction) => void;
 	getProfileAction: (req: Request, res: Response, next: NextFunction) => void;
